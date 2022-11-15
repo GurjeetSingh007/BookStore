87535ec9a058ff53199219e0d6f813a9fb6d8045
GurjeetSingh007 <105614607+GurjeetSingh007@users.noreply.github.com>
2022-11-15 07:21:48 -0800

    removed savechanges method

--- GurjeetsBooks.DataAccess/Repository/CategoryRepository.cs
+++ GurjeetsBooks.DataAccess/Repository/CategoryRepository.cs
@@ -25,7 +25,6 @@ namespace GurjeetsBooks.DataAccess.Repository
             if (objFromDb == null) // save changes if not null
             {
                 objFromDb.Name = category.Name;
-                _db.SaveChanges();
             }
         }
     }
