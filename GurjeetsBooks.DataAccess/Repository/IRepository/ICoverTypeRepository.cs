3e2ee349c79e1a8b1d511992b63740ce4ce7ac4a
GurjeetSingh007 <105614607+GurjeetSingh007@users.noreply.github.com>
2022-11-22 00:23:21 -0500

    ICoverTypeRepository interface Added

---
+++ GurjeetsBooks.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using GurjeetsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GurjeetsBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType coverType);
+    }
+}
